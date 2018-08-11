31805b20285de6a974eb3d635d4b24da896ed740
Teemu Salo <teemusalot@gmail.com>
2018-08-11 18:09:10 +0300

    excercises until 1.9 done. create 3 views next

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     }
 
     // herkkuja: computed property, previous state
-    // Prev Staten avulla uutta arvoa ei tarvitse argumenttina ja
+    // Prev Staten avulla uutta arvoa ei tarvitse argumenttina
     // huolehtii myös mahdollisesta data racesta
     handleClick = (stateProp) => () => {
         this.setState((prevState) => ({
@@ -20,9 +20,75 @@ class App extends React.Component {
         }));
     }
 
+    keskiarvo = () => {
+
+        const avg = (this.state.good - this.state.bad) / (this.state.good + this.state.neutral + this.state.bad)
+
+        const notNumber = isNaN(avg)
+
+        return (
+            // Palauta nolla jos ei vielä klikkauksia tai pyöristä kahteen desimaaliin
+            notNumber ? 0 : avg.toFixed(2)
+        )
+    }
+
+    positiivisia = () => {
+
+        const pos = (this.state.good) / (this.state.good + this.state.neutral + this.state.bad) * 100
+
+        const notNumber = isNaN(pos)
+
+        return (
+            // Palauta nolla jos ei vielä klikkauksia tai pyöristä kahteen desimaaliin
+            notNumber ? 0 : pos.toFixed(1)
+        )
+    }
+
+    Statistic = ({text, arvo, suffix}) => {
+        // Suffix: lisää perään esim % merkki jos arvo annettu
+        return (
+            <tr >
+                <td style={{paddingRight:'10px'}}>{text}</td><td>{arvo}{suffix ? ' ' + suffix : ''}</td>
+            </tr>
+        )
+    }
+
+    // Componettien tekoa kokeiltu class methodina. Arrow function ymmärtää ilmeisesti täällä this:iä
+    // Kannattaako lokaalit componentit tehdä class methodina vai renderissä? Netissä ei kerrota
+    Statistics = () => {
+
+        const valsGiven = this.state.good + this.state.neutral + this.state.bad
+
+        if (valsGiven !== 0) {
+            return (
+                <div>
+                    <h3>Statistics</h3>
+                    <table style={{borderSpacing:'0px'}}>
+                        <tbody>
+                            <this.Statistic text='hyvä' arvo={this.state.good}/>
+                            <this.Statistic text='neutraali' arvo={this.state.neutral}/>
+                            <this.Statistic text='huono' arvo={this.state.bad}/>
+                            <this.Statistic text='keskiarvo' arvo={this.keskiarvo()}/>
+                            <this.Statistic text='positiivisia' arvo={this.positiivisia()} suffix='%'/>
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <h3>Statistics</h3>
+                    <p>Palautetta ei vielä annettu</p>
+                </div>
+            )
+        }
+
+
+    }
+
     render(){
 
-        // Button pitää luoda renderissä, class property ei voi olla react component(?)
         // callback ei tarvitse argumentteja sillä render() palauttaa funktion,
         // jossa argumentit on valmiiksi kutsuttu
         const Button = ({ text, callback }) => {
@@ -33,10 +99,6 @@ class App extends React.Component {
             )
         }
 
-        // Perusta viela nama componentit
-        // Statistics huolehtii tilastojen näyttämisestä
-        // Statistic huolehtii yksittäisen tilastorivin, esim. keskiarvon näyttämisesta
-
         return (
             <div>
                 <h1>Anna Palautetta!</h1>
@@ -45,10 +107,7 @@ class App extends React.Component {
                 <Button text='neutraali' callback={this.handleClick('neutral')} />
                 <Button text='huono' callback={this.handleClick('bad')} />
 
-                <h2>statistiikka</h2>
-                <p>hyvä {this.state.good}</p>
-                <p>neutraali {this.state.neutral}</p>
-                <p>huono {this.state.bad}</p>
+                <this.Statistics/>
             </div>
         )
     }
